91cb4b7002d9f8201d35a6214913db133fefcb6f
iszekeres-so <iszekeres.x@gmail.com>
2015-11-17 13:51:34 +0100

    Introducing MenuBarFactory to setup the whole MenuBar.

--- sources/src/main/java/org/crf/tr/TestReporter.java
+++ sources/src/main/java/org/crf/tr/TestReporter.java
@@ -3,15 +3,9 @@
  */
 package org.crf.tr;
 
-import static java.lang.System.out;
-
-import javafx.scene.control.SeparatorMenuItem;
-import javafx.scene.input.KeyCombination;
+import org.crf.tr.ui.MenuBarFactory;
 import javafx.application.Application;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.Menu;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 
@@ -30,64 +24,13 @@ public final class TestReporter extends Application {
 		launch( args );
 	}
 
-
-	final Menu makeFileMenu() {
-		final Menu file = new Menu( "File" );
-		final MenuItem open = new MenuItem( "Open" );
-		open.setOnAction( evt -> {
-			// TODONE: call file chooser && results opener
-			out.println( "Test Results opened.." );
-		});
-		open.setAccelerator(KeyCombination.keyCombination( "Ctrl+Shift+O" ));
-		
-		final MenuItem archive = new MenuItem( "Archive" );
-		archive.setOnAction( evt -> {
-			// TODONE: store to DB
-			out.println( "Test Results saved to db.." );
-		});
-		archive.setAccelerator(KeyCombination.keyCombination( "Ctrl+Shift+S" ));
-	    
-		final MenuItem exit = new MenuItem( "Exit" );
-	    exit.setOnAction( evt -> {
-	    	// TODONE: setup callbacks..
-	    	System.exit( 0 );
-	    });
-	    exit.setAccelerator(KeyCombination.keyCombination( "Ctrl+Q" ));
-	    file.getItems().addAll( open, archive, new SeparatorMenuItem(), exit );
-		return file;
-	}
-
-	final Menu makeRunMenu() {
-		final Menu run = new Menu( "Run" );
-		final MenuItem shellCommand = new MenuItem( "Shell Command" );
-		shellCommand.setOnAction( evt -> {
-			// TODONE: open dialog window..
-			out.println( "Executed in shell..." );
-		});
-		shellCommand.setAccelerator(KeyCombination.keyCombination( "Ctrl+Shift+R" ));
-
-		run.getItems().addAll( shellCommand );
-		return run;
-	}
-	
-	/**
-	 * */
-	final MenuBar makeMenuBar() {
-		final MenuBar menuBar = new MenuBar( );
-		final Menu fileMenu = makeFileMenu( );
-		final Menu runMenu = makeRunMenu( );
-		
-		menuBar.getMenus().addAll( fileMenu, runMenu );
-		return menuBar;
-	}
-
 	/**
 	 * @param primary
 	 * */
 	@Override
 	public final void start(final Stage primary) {
 		final BorderPane main = new BorderPane( );
-		main.setTop(makeMenuBar( ));
+		main.setTop(MenuBarFactory.makeFor( this ));
 		
 		primary.setScene(new Scene( main, 1024, 768 ));
 		primary.show( );
---
+++ sources/src/main/java/org/crf/tr/ui/MenuBarFactory.java
@@ -0,0 +1,79 @@
+/**
+ * MenuBarFactory.java
+ */
+package org.crf.tr.ui;
+
+
+import javafx.scene.control.SeparatorMenuItem;
+import javafx.scene.input.KeyCombination;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.MenuBar;
+import static java.lang.System.out;
+import javafx.scene.control.Menu;
+
+import org.crf.tr.TestReporter;
+
+
+/**
+ *
+ * @author cvirtue
+ * @version "%I, %G"
+ */
+@SuppressWarnings( "restriction" )
+public final class MenuBarFactory {
+
+	static final Menu makeFileMenuFor(final TestReporter owner) {
+		final Menu file = new Menu( "File" );
+		final MenuItem open = new MenuItem( "Open" );
+		open.setOnAction( evt -> {
+			// TODONE: call file chooser && results opener
+			out.println( "Test Results opened.." );
+		});
+		open.setAccelerator(KeyCombination.keyCombination( "Ctrl+Shift+O" ));
+		
+		final MenuItem archive = new MenuItem( "Archive" );
+		archive.setOnAction( evt -> {
+			// TODONE: store to DB
+			out.println( "Test Results saved to db.." );
+		});
+		archive.setAccelerator(KeyCombination.keyCombination( "Ctrl+Shift+S" ));
+	    
+		final MenuItem exit = new MenuItem( "Exit" );
+	    exit.setOnAction( evt -> {
+	    	// TODONE: setup callbacks..
+	    	System.exit( 0 );
+	    });
+	    exit.setAccelerator(KeyCombination.keyCombination( "Ctrl+Q" ));
+	    file.getItems().addAll( open, archive, new SeparatorMenuItem(), exit );
+		return file;
+	}
+
+	static final Menu makeRunMenuFor(final TestReporter owner) {
+		final Menu run = new Menu( "Run" );
+		final MenuItem shellCommand = new MenuItem( "Shell Command" );
+		shellCommand.setOnAction( evt -> {
+			// TODONE: open dialog window..
+			out.println( "Executed in shell..." );
+		});
+		shellCommand.setAccelerator(KeyCombination.keyCombination( "Ctrl+Shift+R" ));
+
+		run.getItems().addAll( shellCommand );
+		return run;
+	}
+	
+	/**
+	 * Produces a <code>MenuBar</code> for (a) TestReporter with the required <code>Menu</code>
+	 * elements to control its behavior.
+	 * 
+	 * @param owner
+	 * */
+	public static final MenuBar makeFor(final TestReporter owner) {
+		final MenuBar menuBar = new MenuBar( );
+		final Menu fileMenu = makeFileMenuFor( owner );
+		final Menu runMenu = makeRunMenuFor( owner );
+
+		menuBar.getMenus().addAll( fileMenu, runMenu );
+		return menuBar;
+	}
+
+}
