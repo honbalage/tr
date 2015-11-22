c033398891588ed9025b79ef2341d962f27d4655
iszekeres-so <iszekeres.x@gmail.com>
2015-11-22 18:14:14 +0100

    Adding more of excluded sources.. Tests for example.

---
+++ sources/src/test/java/org/crf/tr/services/mocks/MockingBoostReportService.java
@@ -0,0 +1,15 @@
+/**
+ * MockingBoostReportService.java
+ */
+package org.crf.tr.services.mocks;
+
+import org.crf.tr.services.BoostReportService;
+
+/**
+ *
+ * @author cvirtue
+ * @version "%I, %G"
+ */
+public final class MockingBoostReportService extends BoostReportService {
+
+}
