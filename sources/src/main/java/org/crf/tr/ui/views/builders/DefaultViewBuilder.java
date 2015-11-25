6c99e0027dd7dbb1e697878558368ad747cac235
iszekeres-so <iszekeres.x@gmail.com>
2015-11-25 17:18:18 +0100

    Introducing DefaultViewBuilder to do the dirty work...

---
+++ sources/src/main/java/org/crf/tr/ui/views/builders/DefaultViewBuilder.java
@@ -0,0 +1,36 @@
+/**
+ * DefaultViewBuilder.java
+ */
+package org.crf.tr.ui.views.builders;
+
+import static org.crf.tr.ui.views.Styles.applyOn;
+import org.crf.tr.ui.views.TestsTrendView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import org.crf.tr.TestReporter;
+import javafx.scene.Node;
+import java.util.Arrays;
+import java.util.List;
+
+
+/**
+ *
+ * @author cvirtue
+ * @version "%I, %G"
+ */
+public final class DefaultViewBuilder {
+
+	public static final void buildFor(final TestReporter owner, final Node reportView) {
+		final HBox container = owner.container();
+		container.setSpacing( 7 );
+		final List<Node> views = Arrays.asList( reportView, new TestsTrendView( owner ));
+		final List<Node> children = container.getChildren();
+		children.clear();
+		children.add(views.get( 0 ));
+
+		final VBox chartsHolder = new VBox();
+		applyOn( chartsHolder, "crf-charts-view-holder" );
+		chartsHolder.getChildren().add(views.get( 1 ));
+		children.add( chartsHolder );
+	}
+}
