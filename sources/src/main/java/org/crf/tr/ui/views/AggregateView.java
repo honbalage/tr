98db3a45b3e0a43f9f2c296a98daf591717b72ae
iszekeres-so <iszekeres.x@gmail.com>
2015-11-27 17:02:41 +0100

    Adding AggregateView to hold generic data and basic behavior of Aggregates view, to define uniform layout and styling.

---
+++ sources/src/main/java/org/crf/tr/ui/views/AggregateView.java
@@ -0,0 +1,59 @@
+/**
+ * AggregateView.java
+ */
+package org.crf.tr.ui.views;
+
+
+import static org.crf.tr.ui.views.Styles.applyCommons;
+import static org.crf.tr.ui.views.Styles.applyOn;
+import javafx.scene.chart.Chart;
+import javafx.scene.layout.VBox;
+import org.crf.tr.TestReporter;
+
+
+/**
+ *
+ * @author cvirtue
+ * @version "%I, %G"
+ */
+public abstract class AggregateView <C extends Chart> extends VBox implements Viewable {
+
+	protected AggregateView(final TestReporter owner) {
+		super( );
+		this._owner = owner;
+		this._aggregateChart = makeChart( _owner );
+		initWidgets( );
+	}
+
+	private final void initWidgets() {
+		applyCommons( this );
+		applyOn( this, "crf-aggregate-view" );
+		buildHeaderSection( );
+		buildContentSection( );
+		buildFooterSection( );
+	}
+
+	protected abstract void buildHeaderSection();
+	protected abstract void buildContentSection();
+	protected void buildFooterSection() {
+	}
+
+	@Override
+	public void refresh() {
+		// TODONE Auto-generated method stub		
+	}
+
+	protected abstract C makeChart(final TestReporter owner);
+
+
+	public final C chart() {
+		return _aggregateChart;
+	}
+
+	public final TestReporter owner() {
+		return _owner;
+	}
+
+	private final TestReporter _owner;
+	private final C _aggregateChart;
+}
